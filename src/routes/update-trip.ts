9af68ab1ceb8f249eb91ced8e94d94490c37b23e
Guilherme Pereira <guifelipedev@gmail.com>
2024-07-10 17:09:39 -0400

    feat(trip): get and update trip

---
+++ src/routes/update-trip.ts
@@ -0,0 +1,67 @@
+import { FastifyInstance } from "fastify"
+import { ZodTypeProvider } from "fastify-type-provider-zod"
+import z from "zod"
+import { prisma } from "../lib/prisma"
+import { dayjs } from "../lib/dayjs"
+import { ClientError } from "../errors/client-error"
+
+export async function updateTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().patch(
+    "/trips/:tripId",
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4).optional(),
+          starts_at: z.coerce.date().optional(),
+          ends_at: z.coerce.date().optional(),
+        }),
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+      },
+    },
+    async (req) => {
+      const { destination, ends_at, starts_at } = req.body
+      const { tripId } = req.params
+
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        },
+      })
+
+      if (!trip) throw new Error("Could not find a trip with this ID .")
+
+      if (!!starts_at) {
+        if (dayjs(starts_at).isBefore(new Date())) {
+          throw new Error("Invalid trip start date.")
+        }
+      }
+
+      if (!!starts_at) {
+        if (dayjs(starts_at).isAfter(ends_at ?? trip.ends_at)) {
+          throw new Error(
+            "You could not change a start date to after the end date."
+          )
+        }
+      }
+
+      if (!!ends_at) {
+        if (dayjs(ends_at).isBefore(starts_at ?? trip.starts_at)) {
+          throw new ClientError("Invalid trip end date.")
+        }
+      }
+
+      await prisma.trip.update({
+        where: { id: tripId },
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+        },
+      })
+
+      return { tripId: trip.id }
+    }
+  )
+}
